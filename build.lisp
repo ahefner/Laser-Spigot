102d53702edf741e95073b3f914c629e6221d019
Andy Hefner <ahefner@gmail.com>
2014-08-20 22:25:51 -0400

    Steal newer version of "BuildBastard" (...) from GGG repo.

--- build.lisp
+++ build.lisp
@@ -1,3 +1,9 @@
+;;;; Ye BuildBastard!
+
+;;;; To disable colorization of compiler messages, set the
+;;;; BUILD_NO_COLORS environment variable.
+
+;;;; -----------------------------------------------------------------
 
 ;;;; Set  global  optimization levels.   I've  learned that  declaimed
 ;;;; optimize  levels  leak between  files  with  ECL's compiler,  but
@@ -5,8 +11,13 @@
 ;;;; effect on  compiled files.  So,  as something of a  hack, declaim
 ;;;; them in a file, and compile that file.
 
-;;;; Hopefully this works as I described, not having anything to do
-;;;; with the compiler possibly not being loaded yet.
+;;;; I don't think I can blame this on the compiler not being loaded
+;;;; yet, since I'm calling compile file.
+
+;;;; Addendum: This behavior may have changed in ECL recently. I
+;;;; haven't looked into it.
+
+(require 'cmp)
 
 (load (compile-file "declaim.lisp"))
 
@@ -39,7 +50,8 @@ for comparison. Accepts and produces only lists."
 
 (compile 'fail)
 
-(setf c::*cc-flags* (format nil "~A~{ ~A~}" c::*cc-flags* (cflags)))
+
+(setf c:*user-cc-flags* (format nil "~{~A ~}" (cflags)))
 
 (defun object-pathname (filename)
   (compile-file-pathname filename :type :object))
@@ -73,22 +85,32 @@ for comparison. Accepts and produces only lists."
         do (setf index end)
         unless index do (return (nreverse accum))))
 
-(defun c-object-deps (filename)
-  (let* ((args (append (cflags) (list "-M" (namestring filename))))
-         (stream (ext:run-program "cpp" args
+(defun c-object-deps (filename &optional (compiler "gcc"))
+  ;; We have to use "gcc -E" instead of "cpp" due to some stupidity on
+  ;; OS X where cpp barfs on variadic macros in the system header
+  ;; files, because it passes -traditional to GCC. Whatever.
+  (let* ((args `("-E"
+                 ;; Suggested for pre-Snow Leopard OS X, not sure if I need it:
+                 ;; "-traditional-cpp"
+                 ,@(cflags)
+                 "-M" ,(namestring filename)))
+         (stream (ext:run-program compiler args
                                   :input nil :output :stream :error *error-output*)))
-    (handler-case 
+    (handler-case
         (progn
           (loop as char = (read-char stream nil)
 		until (eql char #\:)
 		when (null char)
 		;; This fails on a certain file in Windows. I'm not
 		;; sure why, but easiest to work around it here:
-		do (warn "Problem reading CPP output!") (return-from c-object-deps (list filename)))
+		do
+                (warn "Problem reading CPP output! Command invocation:~%   ~{~A ~}"
+                      (cons compiler args))
+                (return-from c-object-deps (list filename)))
           (peek-char t stream nil)
           (split-string (read-unescaping stream)))
       (serious-condition (err)
-        (format t "~&~A ~{~A~^ ~}~%" "cpp" args)
+        (format t "~&~A ~{~A~^ ~}~%" compiler args)
         (format t "~&Error computing dependencies of ~A:~% ~A~%" filename err)
         (fail)))))
 
@@ -103,15 +125,17 @@ for comparison. Accepts and produces only lists."
   (remove-if (lambda (dep) (newer? (object-pathname pathname) (pathname dep)))
              (append (file-dependencies pathname) extra-deps)))
 
+(defun dont-colorize () (si:getenv "BUILD_NO_COLORS"))
+
 ;;;; Build process
 
 (defvar *compiler-sources* (make-hash-table :test 'equal))
 
-(defun ensure-compiler-source (filename)  
+(defun ensure-compiler-source (filename)
   (unless (gethash filename *compiler-sources*)
     (format t "~&~%----- Compile-time dependency: ~A -----~%" filename)
-    (load (compile-file filename :print nil :verbose nil 
-                        :output-file (format nil "compile-time-~A.fasl" 
+    (load (compile-file filename :print nil :verbose nil
+                        :output-file (format nil "compile-time-~A.fas"
                                              (pathname-name (pathname filename)))))
     (setf (gethash filename *compiler-sources*) t)))
 
@@ -129,7 +153,8 @@ for comparison. Accepts and produces only lists."
 (defvar *use-colors* #-win32 t #+win32 nil)
 (defun sgr (&rest modes)
   "Print ANSI color codes. Doesn't work in the Windows console."
-  (when *use-colors* (format t "~C[~{~D~^;~}m" #\Esc modes)))
+  (when (and *use-colors* (not (dont-colorize)))
+    (format t "~C[~{~D~^;~}m" #\Esc modes)))
 
 (loop with compiler-sources-loaded = nil
       for source-spec in (append (c-sources) (lisp-sources))
@@ -137,7 +162,7 @@ for comparison. Accepts and produces only lists."
                         (first source-spec)
                         source-spec)
       as compile-time-deps = (and (listp source-spec) (rest source-spec))
-      as deps = (changed-dependencies (pathname filename) compile-time-deps)      
+      as deps = (changed-dependencies (pathname filename) compile-time-deps)
       ;;do (print (list :file filename :compile-time-deps compile-time-deps :deps deps))
       when deps
       ;;; Color-code the compiler warnings.
@@ -155,19 +180,21 @@ for comparison. Accepts and produces only lists."
                          (lambda (condition)
                            (sgr 1 31))))
            (sgr 0)
-           (unless compiler-sources-loaded 
+           (unless compiler-sources-loaded
              (setf compiler-sources-loaded t)
              (load-compiler-sources)
-             (sgr 0)
-             (format t "~&~%--------------- BUILDING SOURCE FILES ---------------~%~%"))
-           
+             (sgr 37)
+             (format t "~&~%--------------- BUILDING SOURCE FILES ---------------~%~%")
+             (sgr 0))
+
            (when (probe-file (object-pathname filename))
              (sgr 0)
              (format t "~&(Recompiling due to changes in ~{~A~^, ~})~%" deps))
            (cond
              ((c-file? filename)
-              (sgr 0)
+              (sgr 37)
               (format t "~&~%----===---- Compiling ~A ----===----~%" filename)
+              (sgr 0)
               (handler-bind
                   ((serious-condition (lambda (foo) (fail))))
                 (sgr 0)
@@ -177,15 +204,21 @@ for comparison. Accepts and produces only lists."
                 (ext:quit 1)))
              (t                         ; Lisp file
               (mapc #'ensure-compiler-source compile-time-deps)
-              (sgr 0)
+              (sgr 37)
               (format t "~&~%----===---- Compiling ~A ----===----~%" filename)
-              (unless (compile-file filename 
+              (sgr 0)
+              (unless (compile-file filename
                                     :verbose nil :system-p t :print nil
-                                    :external-format :latin-1
+                                    ;;:external-format :latin-1
                                     :c-file (make-pathname
                                              :type "c"
                                              :directory (pathname-directory #p"obj/")
-                                             :name (pathname-name (pathname filename))))
+                                             :name (pathname-name (pathname filename)))
+                                    #|
+                                    :h-file (make-pathname
+                                             :type "h"
+                                             :directory (pathname-directory #p"obj/")
+                                             :name (pathname-name (pathname filename)))|#)
                 (sgr 0 31)
                 (format *trace-output* "~&Error compiling ~A~%" filename)
                 (sgr 0)
@@ -198,7 +231,7 @@ for comparison. Accepts and produces only lists."
 (defun source-names->object-names (sources)
   (mapcar #'object-pathname sources))
 
-(c:build-program #+win32 "g3.exe" #-win32 "g3"
+(c:build-program #+win32 "g1.exe" #-win32 "g1"
                   :lisp-files (source-names->object-names (lisp-linked-sources))
                   :ld-flags (append (mapcar #'namestring (source-names->object-names (c-sources)))
 				    (ld-flags)
